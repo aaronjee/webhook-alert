919f9ab8f749f0644c089670dacb6829e362e32d
aaronjee <104844540+aaronjee@users.noreply.github.com>
2025-09-13 19:59:44 +0900

    Create app-webhook.py

---
+++ app-webhook.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+import json
+import os
+import uuid
+import requests
+from datetime import datetime
+
+app = Flask(__name__)
+
+# 외부 메신저 API 주소
+EXTERNAL_MESSENGER_API = "http://xxx.xxx.xxx.xxx/api"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # json payload 수신
+    alert_json = request.json 
+
+    # 메세지 가공
+    alerts = alert_json.get('alerts', [])
+    messages = []
+  
+    for alert in alerts:
+        labels = alert.get('labels', {})
+        annotations = alert.get('annotations', {})
+        msg = f"Alert" {labels.get('alertname')} - {annotations.get('summary', '')}"
+        message_text = "\n.join(messages)
+
+        # 송신결과 출력
+        print(messages_text)
+
+    # x-www-form-urlencoded 형식으로 메세지 생성
+    form_data = {
+      "SRV_CODE" : BSRD03",
+      "SEND": "xxxxxx",
+      "TITLE": "Alert",
+      "BODY": message_text
+    }
+
+    # message 전송
+    response = requests.post(
+      EXTERNAL_MESSENGER_API,
+      data = form_data,
+      headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+
+    return f"Sent message with status {response.status_code}"
+  
+@app.route('/')
+def hello():
+    return "You should call /webhook"
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
